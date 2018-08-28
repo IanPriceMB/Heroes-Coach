3911b56454ca917c56aba88e1587d690bc745ab3
Ian Price <ianpriceve@gmail.com>
2018-08-27 19:16:00 -0600

    initial header styles

--- scripts/characters.js
+++ scripts/characters.js
@@ -1,11 +1,11 @@
 const characters = [
 'abathur', 'alarak', 'alexstrasza', 'ana', 'anubarak', 'artanis', 'arthas', 'auriel', 
 'azmodan', 'blaze', 'brightwing', 'cassia', 'chen', 'chogall', 'chromie', 'dva', 'deckard',
-'dehaka', 'diablo', 'etc', 'falstad', 'fenix', 'garrosh', 'garrosh', 'gazlowe', 'genji', 'greymane',
+'dehaka', 'diablo', 'etc', 'falstad', 'fenix', 'garrosh', 'gazlowe', 'genji', 'greymane',
 'guldan', 'hanzo', 'illidan', 'jaina', 'johanna', 'junkrat', 'kaelthas', 'kelthuzad', 'kerrigan',
 'kharazim', 'leoric', 'li-li', 'li-ming', 'lt-morales', 'lucio', 'lunara', 'maiev', 'malfurion', 
 'malthael', 'medivh', 'muradin', 'murky', 'nazeebo', 'nova', 'probius', 'ragnaros', 'raynor', 
-'rehgar', 'rexxar', 'samuro', 'sgt-hammer', 'sonya', 'stiches', 'stukov', 'sylvanas', 'tassadar',
+'rehgar', 'rexxar', 'samuro', 'sgt-hammer', 'sonya', 'stitches', 'stukov', 'sylvanas', 'tassadar',
 'the-butcher', 'the-lost-vikings', 'thrall', 'tracer', 'tychus', 'tyrael', 'tyrande', 'uther',
 'valeera', 'valla', 'varian', 'whitemane', 'xul', 'yrel', 'zagara', 'zarya', 'zeratul', 'zuljin' 
 ];
@@ -13,5 +13,5 @@ const characters = [
 for (let i = 0; i<characters.length; i++){
     var newCharacter = $('<img>');
     $(newCharacter).attr('src', 'staticFiles/heroPics/' + characters[i] + '.png');
-    $('body').append(newCharacter);
+    $('.map').append(newCharacter);
 }
