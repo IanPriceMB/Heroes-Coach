782380982d22235399fdb7c7972f24a0af7e5458
Ian Price <ianpriceve@gmail.com>
2018-09-05 00:29:53 -0600

    hotkeys and about sections functionally fixed still not styled

--- scripts/hotkeys.js
+++ scripts/hotkeys.js
@@ -52,19 +52,23 @@ $(document).on("keypress",function(e) {
         if($('.about-div').length == 0){
             $('body').append('<div class="about-div">')
             $('.about-div').append('<p class="about-p">')
-            $('.about-p').html(`Over Coach is a project I made for my web design class.
+            $('.about-p').html(`Hello there, I am MaddBuddha, a Masters Division player and shot caller for the team Off-Brand Cereal. I've also coached for several collegiate teams in
+            Heroes of the Dorm, League of Legends and DOTA2. I currently offer private lessons and I thought this was a great way to help my students learn!
             <br>
             <br>
-            If you support this project on Patreon I will continue to update it with more content.
+            If you support this project on patreon I will continue to update it and make it a little better every day. The more money you pledge, the better the updates will be.
+            I have a lot of plans for the future already! So, if you are really like this tool then please consider supporting the project.
             <br>
             <br>
-            Check the hotkeys section for a menu of quick actions to improve your experience.
+            Be sure to subscribe to my YouTube channel (<a href=${'https://www.youtube.com/channel/UCaSkA7hiJ4PewjRrFo1IYgg?view_as=subscriber'} target=${'_blank'}>click me!</a>),
+            where I post daily content including: coaching videos, solo que VOD's, tournament and league shot calling VOD's, and much more! 
+            I hope you enjoy my Heroes of the Storm coaching tool! 
             <br>
             <br>
-            I hope you enjoy Over Coach!
+            I can't recommend the hotkeys section enough! If you plan on using this a lot it's definately worth a look.
             <br>
             <br>
-            -Ian Price
+            -Ian 'MaddBuddha' Price
             <br>
             <br>`)
             $('.about-div').append('<button class="closeBtn">')
@@ -86,7 +90,7 @@ $(document).on("keypress",function(e) {
             T = Eraser<br><br>Ctrl+Click = Clear Hero From the Board<br><br>L = Clear All Lines<br><br>H = Reset All Heroes
             <br>
             <br>
-            B = Clear the Board<br><br>M = Maps List<br><br>A = About Popout<br><br>Z = Hotkeys
+            C = Character Selection sScreen<br><br>B = Clear the Board<br><br>M = Maps List<br><br>A = About Popout<br><br>Z = Hotkeys
             <br>
             <br>`)
             $('.hotkey-div').append('<button class="closeBtn2">')
